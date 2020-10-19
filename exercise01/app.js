fa409a5c1a3813d43ea197ac8acb81680a5b9a12
Lypzis <victorvp2018@protonmail.com>
2020-10-19 10:04:23 -0300

    Update

---
+++ exercise01/app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			name: 'Victor',
+			age: 25,
+			imageUrl: 'https://i.ytimg.com/vi/5qfzqNtfP_Q/hqdefault.jpg',
+		};
+	},
+	methods: {},
+});
+
+app.mount('#assignment');
